180762e0f265c150b21f54094fe4b2d28e4a41dd
Sasha Ermohin <saha22023@gmail.com>
2020-12-21 17:26:28 +0700

    No duplications after reload page with post (made form checks...)

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView
 from django.views.generic import DetailView
+from django.views.generic.edit import FormMixin
+from django.shortcuts import redirect
 from .models import Post
 from .models import Comment
 from .forms import CommentForm
@@ -10,23 +12,38 @@ class BlogListView(ListView):
     template_name = 'page_index.html'
 
 
-class BlogDetailView(DetailView):
+class BlogDetailView(FormMixin, DetailView):
     model = Post
     template_name = 'page_post.html'
+    form_class = CommentForm
     
     def get_context_data(self, **kwargs):
         data = super(BlogDetailView, self).get_context_data(**kwargs)
-        comments = Comment.objects.filter(post=self.get_object()).order_by('-created_at')
+        comments = Comment.objects.filter(
+            post=self.get_object()
+        ).order_by('-created_at')
         data['comments'] = comments
         data['comment_form'] = CommentForm()
         return data
     
     def post(self, request, *args, **kwargs):
-        # TODO: prevent comment duplication after reloading page. (CSRF?)
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+    
+    def form_valid(self, form):
+        if not self.request.user.is_authenticated:
+            return redirect('post', self.kwargs['pk'])
+        
         comment = Comment(
-            content=request.POST.get('content'),
+            post=self.get_object(),
             author=self.request.user,
-            post=self.get_object()
+            content=form.cleaned_data.get('content')
         )
         comment.save()
-        return self.get(self, request, *args, **kwargs)
+        return redirect('post', self.kwargs['pk'])
+    
+    def form_invalid(self, form):
+        return redirect('post', self.kwargs['pk'])
